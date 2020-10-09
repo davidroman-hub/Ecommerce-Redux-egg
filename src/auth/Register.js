527e26ec27574e5ae16e99f0cbc4af9a00e28624
David <jobroman83@gmail.com>
2020-10-09 16:37:58 +0200

    task 4 setUp form part1

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 
 const Register = () => {
-    return (
-        <div>
-            <h1>Hello Register</h1>
+
+    const [email,setEmail] = useState('')
+
+
+    const handleSubmit = () => {
+        //
+    }
+
+
+    const registerFormExec = () => (
+        <form onSubmit = {handleSubmit}> 
+            <input type='email' 
+                className='form-control'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoFocus
+                />
+                <button type='submit' className='btn btn-raised mt-4'> 
+                    Register
+                </button>
+        </form>
+    )
+
+
+
+const registerForm = () => (
+    <div className='container p-5'>
+        <div className='row'>
+            <div className=' col-md-6 offset-md-3'>
+                <h4>Register</h4>
+                {registerFormExec()}
+            </div>
         </div>
+    </div>
+)
+
+
+
+    return (
+        <>
+            <div>
+                {registerForm()}
+            </div>
+        </>
     )
 }
 
