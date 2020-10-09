5a60fcfa198b341fa92691ad2f5f269937c7ffcc
David <jobroman83@gmail.com>
2020-10-09 14:25:05 +0200

    task 1 Routing the fist pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Auth
+import { Login } from './auth/Login';
+import { Signin } from './auth/Signin';
+
+const App = () => {
+  return(
+    <>
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/Signin' component={Signin}/>
+              <Redirect to='/login' /> 
+            </Switch>
+          </div>
+        </Router>
+    </>
+  )
 }
-
 export default App;
