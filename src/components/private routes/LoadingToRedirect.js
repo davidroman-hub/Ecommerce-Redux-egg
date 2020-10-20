7112ce722afdc11d64561192d534f518f5e51176
David <jobroman83@gmail.com>
2020-10-20 13:46:19 +0200

    task 27 Redirect countdown in protect routes

---
+++ src/components/private routes/LoadingToRedirect.js
@@ -0,0 +1,27 @@
+import React,{useState, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+
+const LoadingToRedirect = () => {
+    const [count, setCount] = useState(3)
+    let history = useHistory()
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            //dicrement coutn
+            setCount((currentCount) => --currentCount);
+        },1000);
+        //Redirect onbce count is equal to 0
+        count === 0 && history.push('/');
+        //cleanup
+        return () => clearInterval(interval)
+    },[count])
+
+    return (
+        <div className='container p-5 text-center'>
+            <h3 className='text-danger'>You are not logged!</h3>
+            <p> Redirecting you in <i class="fas fa-spinner fa-pulse"/> {count}  seconds..</p>
+        </div>
+    )
+}
+
+export default LoadingToRedirect
