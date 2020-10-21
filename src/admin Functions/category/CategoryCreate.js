71cd44906e4bc996eae1e108f453d5ca6e9c2bbd
David <jobroman83@gmail.com>
2020-10-21 17:52:36 +0200

    task 34 create category component send info to console log

--- src/admin Functions/category/CategoryCreate.js
+++ src/admin Functions/category/CategoryCreate.js
@@ -1,18 +1,63 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import AdminNav from '../../components/nav/AdminNav';
-const CategoryCReate = () => {
-    return (
-        <div className='container-fluid'>
-        <div className='row'>
-            <div className='col-md-2'>
-                <AdminNav/>
+import Swal from 'sweetalert2';
+import { useSelector } from 'react-redux'
+import { 
+            createCategory, 
+            getCategories, 
+            removeCategory 
+        } from '../../functions/categories';
+
+        
+const CategoryCreate = () => {
+
+    const [name, setName] = useState('')
+
+
+    const handleSubmit = (e) => {
+        //
+        e.preventDefault()
+        console.log(name)
+    }
+
+
+    const showCategoryForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className ='form-group'>
+                <label>Name</label>
+                <input type='text' 
+                        className='form-control' 
+                        onChange={e => setName(e.target.value)} 
+                        value={name}
+                        autoFocus
+                        required
+                        />
+                        <br/>
+                        <button className='btn btn-outlined-primary'>Save</button>
             </div>
-            <div className='col'>
-                Category create
+        </form>
+    )
+
+
+    const CategoryForm = () => (
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-md-2'>
+                    <AdminNav/>
+                </div>
+                <div className='col'>
+                    <h4>Create Category</h4>
+                    {showCategoryForm()}
+                </div>
             </div>
         </div>
-    </div>
+    )
+    
+    return (
+        <>
+            {CategoryForm()}
+        </>
     )
 }
 
-export default CategoryCReate
+export default CategoryCreate
